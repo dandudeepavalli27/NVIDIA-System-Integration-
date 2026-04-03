d8ec3cef9871ebfc40708d542ce40c8d0f0b91da
Deepavalli <dandudeepa27@gmail.com>
2026-04-03 12:12:28 +0530

    Create System Integration.py

---
+++ System Integration.py
@@ -0,0 +1,14 @@
+# SESSION 31 – System Integration
+
+# Step 1: Initialize modules
+perception = True
+planning = True
+control = True
+
+# Step 2: Check integration
+if perception and planning and control:
+    print("Autonomous Execution Successful")
+else:
+    print("System Integration Failed")
+
+print("\nProgram Executed Successfully")
